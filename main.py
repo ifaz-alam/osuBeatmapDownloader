de5b4c65892769a6f979411bca50f32c674772f7
Ifaz A <45114241+ifaz-alam@users.noreply.github.com>
2022-01-06 21:05:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+import requests
 
 PATH = 'C:/Users/Ifaz/Downloads/chromedriver_win32/chromedriver.exe'
 browser = webdriver.Chrome(PATH)
@@ -22,7 +23,13 @@ for link in links:
     # download the beatmap corresponding to the beatmap ID
     # by default n = 1 corresponds to no video associated to the .osz file
     # set n = 0 if you would like to download .osz with video
-    browser.get('https://api.chimu.moe/v1/download/' + beatmapID + "?n=1")
+    response = requests.get('https://api.chimu.moe/v1/download/' + beatmapID + '?n=1')
+    if (response.status_code == 404):
+        # beatmap has not been found on Chimu.moe beatmap host, try alternative host
+        print('This beatmap is not available on Chimu.moe! Attempting to retrieve from Sayobot osu! beatmap mirror')
+        browser.get('https://usa1.sayobot.cn/beatmaps/135/0684/novideo?filename=' + beatmapID)
+    else:
+        browser.get('https://api.chimu.moe/v1/download/' + beatmapID + '?n=1')
     count += 1
     print('%d/%d beatmap listings processed' % (count, len(lines)))
     # delay in seconds
